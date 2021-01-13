9dd1fe55ec1386a5a6703fdf50ce77a0d394d436
Anton Naydenov <naydenov@rumex.ru>
2021-01-13 12:45:39 +0300

    add card

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const homeRoutes = require('./routes/home');
 const cardRoutes = require('./routes/card');
 const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
+const path = require('path');
 
 const app = express();
 
@@ -18,7 +19,7 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 
 app.use(homeRoutes);
